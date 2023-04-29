b098301b3927737266d852d51f6cd337bd7b0108
Evgeniy Sterekhov <john2331832@gmail.com>
2023-04-29 15:28:04 +0500

    Create API functions for events

--- app/src/main/java/ru/stan/nework/data/repository/EventRepositoryImpl.kt
+++ app/src/main/java/ru/stan/nework/data/repository/EventRepositoryImpl.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import ru.stan.nework.data.datasources.EventsRemoteDataSource
 import ru.stan.nework.di.IoDispatcher
 import ru.stan.nework.domain.models.network.NetworkState
+import ru.stan.nework.domain.models.network.event.EventModel
 import ru.stan.nework.domain.models.ui.event.Event
 import ru.stan.nework.domain.repository.EventsRepository
 import ru.stan.nework.utils.safeApiCall
@@ -18,6 +19,35 @@ class EventRepositoryImpl @Inject constructor(
         return safeApiCall(ioDispatcher) {
             remoteDataSource.getEvents()
                 .map { it.convertTo() }
+        }
+    }
+    override suspend fun addEvent(event: Event): NetworkState<EventModel> {
+        return safeApiCall(ioDispatcher) {
+            remoteDataSource.addEvent(event)
+        }
+    }
+
+    override suspend fun removeEvents(id: Long): NetworkState<EventModel> {
+        return safeApiCall(ioDispatcher) {
+            remoteDataSource.removeEvents(id)
+        }
+    }
+
+    override suspend fun getEventById(id: Long): NetworkState<EventModel> {
+        return safeApiCall(ioDispatcher) {
+            remoteDataSource.getEventById(id)
+        }
+    }
+
+    override suspend fun deleteEventLike(id: Long): NetworkState<EventModel> {
+        return safeApiCall(ioDispatcher) {
+            remoteDataSource.removeEvents(id)
+        }
+    }
+
+    override suspend fun addEventLike(id: Long): NetworkState<EventModel> {
+        return safeApiCall(ioDispatcher) {
+            remoteDataSource.addEventLike(id)
         }
     }
 }
--- app/src/main/java/ru/stan/nework/providers/datasources/EventsRemoteDataSourceImpl.kt
+++ app/src/main/java/ru/stan/nework/providers/datasources/EventsRemoteDataSourceImpl.kt
@@ -2,11 +2,33 @@ package ru.stan.nework.providers.datasources
 
 import ru.stan.nework.data.datasources.EventsRemoteDataSource
 import ru.stan.nework.domain.models.network.event.EventModel
+import ru.stan.nework.domain.models.ui.event.Event
 import ru.stan.nework.providers.network.NetworkService
 import javax.inject.Inject
 
-class EventsRemoteDataSourceImpl @Inject constructor(private val apiService: NetworkService): EventsRemoteDataSource {
+class EventsRemoteDataSourceImpl @Inject constructor(private val apiService: NetworkService) :
+    EventsRemoteDataSource {
     override suspend fun getEvents(): List<EventModel> {
         return apiService.getEvents()
+    }
+
+    override suspend fun addEvent(event: Event): EventModel {
+        return apiService.addEvent(event)
+    }
+
+    override suspend fun removeEvents(id: Long): EventModel {
+        return apiService.removeEvent(id)
+    }
+
+    override suspend fun getEventById(id: Long): EventModel {
+        return apiService.getEventById(id)
+    }
+
+    override suspend fun deleteEventLike(id: Long): EventModel {
+        return apiService.deleteEventLike(id)
+    }
+
+    override suspend fun addEventLike(id: Long): EventModel {
+       return apiService.likeEventById(id)
     }
 }
